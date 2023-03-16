ebfa43bed683496bd980d10a95b37d82b27dc249
NinzizaRonie <ninzizaronie0@gmail.com>
2023-03-16 10:11:12 +0300

    update

---
+++ main.js
@@ -0,0 +1,9 @@
+   // document.getElementById("coutn").innerText = 5 
+   let count = 0
+   console.log(count)
+
+   // 1. Create a variable, myAge, and set its value to your age
+   let myAge= 35
+
+   //2. Log the myAge to the console 
+   //console.log(myAge)
